d60d0a0a278b68839110e01c56e67a3bd7b64def
unknown <mcpherson.z@outlook.com>
2023-02-16 11:07:48 -0600

    finished question_9

--- question_9.sql
+++ question_9.sql
@@ -1,33 +1,29 @@
 -- 9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? 
 -- Give their full name and the teams that they were managing when they won the award.
 
-WITH nl_award AS (SELECT playerid
-				  FROM awardsmanagers
+
+
+WITH nl_award AS (SELECT playerid, yearid AS nlyear, teamid AS nl_team
+				  FROM awardsmanagers INNER JOIN managers USING(playerid, yearid)
 				  WHERE awardid LIKE 'TSN%'
 							AND yearid > 1985
-		 					AND lgid LIKE 'NL'),
+		 					AND awardsmanagers.lgid LIKE 'NL'),
 		
-		al_award AS (SELECT playerid
-					 FROM awardsmanagers
+		al_award AS (SELECT playerid, yearid AS alyear, teamid AS al_team
+					 FROM awardsmanagers INNER JOIN managers USING(playerid, yearid)
 					 WHERE awardid LIKE 'TSN%'
 					 AND yearid > 1985
-					 AND lgid LIKE 'AL'),
-					 
-		managers AS ((SELECT *
-				  	FROM nl_award)
-				  	INTERSECT
-				 	(SELECT *
-				  	FROM al_award))
+					 AND awardsmanagers.lgid LIKE 'AL')
 
-SELECT namefirst, namelast, awardid, awardsmanagers.yearid, awardsmanagers.lgid
-FROM managers LEFT JOIN awardsmanagers USING(playerid)
-				LEFT JOIN people USING(playerid)
-				LEFT JOIN teams USING(yearid)
-WHERE awardid LIKE 'TSN%'
-GROUP BY namefirst, namelast, awardid, awardsmanagers.yearid, awardsmanagers.lgid
-ORDER BY yearid;
+SELECT namefirst, namelast, alyear, al_team, nlyear, nl_team
+FROM nl_award INNER JOIN al_award USING(playerid)
+				INNER JOIN people USING(playerid)
+				
 
-	
+
+
+
+
 			
 			
 			
